32781b90288a6b66da349f8026dab1b9e15a6f95
Benjamin Springer <benjamin.springer98@gmail.com>
2021-03-28 17:35:30 +0200

    added main functionality, todo: styling and responsiveness

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
-generateGrid();
+generateGrid(16); //default 16x16 starting grid
 
-const squares = document.querySelectorAll(".square");
+let squares = document.querySelectorAll(".square");
 squares.forEach((square) => {
     square.addEventListener("mouseover", dyeSquare);
 });
 
-function generateGrid() {
+const changeSizeBtn = document.querySelector("#changeSizeBtn");
+changeSizeBtn.addEventListener("click", function() {
+    let size = prompt("Please enter a new size:");
+    removeCurrentSquares();
+    generateGrid(size);
+});
+
+function generateGrid(size) {
+    let squareWidth = Math.sqrt(250000 / (size * size));
+    console.log(squareWidth);
+    console.log(size);
     const containerDiv = document.querySelector("#container");
-    for(let i = 1; i <= 16; i++) {
-        for(let j = 1; j <= 16; j++) {
+    for(let i = 1; i <= size; i++) {
+        for(let j = 1; j <= size; j++) {
             const square = document.createElement("div");
             square.classList.add("square");
+            containerDiv.setAttribute("style", "grid-template-columns: repeat(" + size + "," + squareWidth + "px); grid-template-rows: repeat(" + size + "," + squareWidth + "px)");
             containerDiv.appendChild(square);
         }
-    } 
+    }
+    let squares = document.querySelectorAll(".square"); // set squares to include the newly generated ones
+    squares.forEach((square) => {
+        square.addEventListener("mouseover", dyeSquare);
+    }); 
 }
 
 function dyeSquare() {
-    this.setAttribute("style", "background-color: red");
+    let color = document.getElementById("color").value; // get color from html input
+    this.setAttribute("style", "background-color:" + color + "");
+}
+
+function removeCurrentSquares() {
+    let currentSquares = document.querySelectorAll(".square");
+    currentSquares.forEach((currentSquare) => {
+        currentSquare.remove();
+    });
 }
 
